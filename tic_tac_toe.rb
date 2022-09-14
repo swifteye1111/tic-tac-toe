b1a31255042c225b749ee5c78b3e4cbf5d738c8d
swifteye1111 <hasak.josh@gmail.com>
2022-09-14 11:45:35 +0100

    Put make board and display board into their own functions.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,20 +6,33 @@
 # --+---+--
 # 7 | 8 | 9
 game_arr = %w[1 2 3 4 5 6 7 8 9]
-row_div = "\n--+---+--\n"
-col_div = ' | '
-game_display = ''
-game_arr.each_with_index do |elm, i|
-  game_display += elm
-  if i == 2 || i == 5
-    game_display += row_div
-  elsif i != 8
-    game_display += col_div
-  end
-end
+
 puts "Please enter Player One's name:"
 user1 = gets.chomp
 puts "Player One's name is #{user1}."
 puts "Please enter Player Two's name:"
 user2 = gets.chomp
 puts "Player Two's name is #{user2}."
+
+def make_new_board
+  game_arr = %w[1 2 3 4 5 6 7 8 9]
+end
+
+def display_board
+  game_arr = make_new_board()
+  row_div = "\n--+---+--\n"
+  col_div = ' | '
+  game_display = ''
+  game_arr.each_with_index do |elm, i|
+    game_display += elm
+    if i == 2 || i == 5
+      game_display += row_div
+    elsif i != 8
+      game_display += col_div
+    end
+  end
+  puts game_display
+end
+
+make_new_board
+display_board
