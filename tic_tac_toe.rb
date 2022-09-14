a38588441d07d322f2b5b544bc3ee453cce00de8
swifteye1111 <hasak.josh@gmail.com>
2022-09-14 12:58:34 +0100

    Create Game and Player objects.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,23 @@
-module TicTacToe
-  class Game
+# frozen_string_literal: true
+
+# Game class
+class Game
+  attr_accessor :board
+
+  def initialize
+    self.board = %w[1 2 3 4 5 6 7 8 9]
   end
 
-  class Player
+end
+
+# Player class
+class Player
+  def initialize(name)
+    @name = name
   end
 end
+
+game = Game.new
+puts game.board
+player1 = Player.new('Josh')
+player2 = Player.new('Jess')
