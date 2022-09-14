6a1f16dc4f3405b50f2f360c36d012c479021c9c
swifteye1111 <hasak.josh@gmail.com>
2022-09-14 12:04:32 +0100

    Add method for getting user input

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,10 +7,12 @@
 # 7 | 8 | 9
 
 puts "Please enter Player One's name:"
-user1 = gets.chomp
+#user1 = gets.chomp
+user1 = 'Player One'
 puts "Player One's name is #{user1}."
 puts "Please enter Player Two's name:"
-user2 = gets.chomp
+#user2 = gets.chomp
+user2 = 'Player Two'
 puts "Player Two's name is #{user2}."
 
 def make_new_board
@@ -18,7 +20,7 @@ def make_new_board
 end
 
 def display_board(board)
-  game_display = ''
+  game_display = "\n"
   board.each_with_index do |elm, i|
     game_display += elm 
     if i == 2 || i == 5
@@ -30,4 +32,17 @@ def display_board(board)
   puts game_display
 end
 
-display_board(make_new_board)
+def get_choice(user)
+  puts "#{user}: Please input a number from 1-9 that matches a space on the game board:"
+  choice = gets.chomp
+  until choice.to_i.between?(1, 9)
+    puts 'Invalid input. Please choose a number between 1-9:'
+    choice = gets.chomp
+  end
+  puts "Good job! You chose #{choice}!"
+  choice
+end
+
+current_board = make_new_board
+display_board(current_board)
+get_choice(user1)
