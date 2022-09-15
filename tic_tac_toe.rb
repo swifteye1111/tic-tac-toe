cc927c196f1fa3a127fa580111bff5a12976036c
swifteye1111 <hasak.josh@gmail.com>
2022-09-15 13:15:40 +0100

    Implement check to see if game is ended or not.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,17 +2,19 @@
 
 # Game class
 class Game
-  attr_accessor :board
+  attr_accessor :board, :win_check
 
   def initialize
-    self.board = %w[1 2 3 4 5 6 7 8 9]
-    self.run_game
+    self.board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @win_check = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                  [1, 5, 9], [3, 5, 7]]
+    run_game
   end
 
   def display_board
     game_display = ''
-    self.board.each_with_index do |elm, i|
-      game_display += elm
+    board.each_with_index do |elm, i|
+      game_display += elm.to_s
       if i == 2 || i == 5
         game_display += "\n--+---+--\n"
       elsif i != 8
@@ -30,8 +32,10 @@ class Game
   end
 
   def play_round(p1, p2)
-    self.update_board(self.get_choice(p1), p1)
-    display_board
+    i = 0
+    until end_game?
+      update_board(get_choice(p1), p1)
+    end
   end
 
   def get_choice(p)
@@ -41,12 +45,26 @@ class Game
       puts 'Invalid input. Please choose a number between 1-9:'
       choice = gets.chomp
     end
-    puts "Good job! You chose #{choice}!"
+    puts "You chose #{choice}!"
     choice
   end
   
   def update_board(choice, p)
     board[choice.to_i - 1] = p.id.odd? ? 'X' : 'O'
+    display_board
+  end
+
+  def end_game?
+    win_check.each do |arr|
+      arr.each do |elm|
+        unless board.include?(elm)
+          arr.delete(elm)
+        end
+        if arr.empty? then return true end
+      end
+    end
+    p win_check
+    false
   end
 end
 
