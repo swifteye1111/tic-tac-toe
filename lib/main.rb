f2e55f744a5f6045168010e25f4c6d94b46d8c53
swifteye1111 <hasak.josh@gmail.com>
2022-12-20 11:58:05 +0100

    Move game execution to main.rb and game files to /lib

---
+++ lib/main.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'tic_tac_toe'
+
+new_game = Game.new
+new_game.run_game
--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,7 +8,6 @@ class Game
     self.board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @win_check = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8],
                   [0, 4, 8], [2, 4, 6]]
-    run_game
   end
 
   def display_board
