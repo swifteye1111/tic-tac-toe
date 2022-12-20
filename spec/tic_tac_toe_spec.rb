d0a87e30b0361cf0e547836ce5704edbd46013ee
swifteye1111 <hasak.josh@gmail.com>
2022-12-20 12:02:55 +0100

    Clean up directory.

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,3 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../lib/game'
+require_relative '../lib/player'
+
 describe Game do
-  desribe display
+  desribe display_board do
+  end
+
+  describe run_game do
+  end
+
+  describe play_round do
+  end
+
+  describe get_choice do
+  end
 end
